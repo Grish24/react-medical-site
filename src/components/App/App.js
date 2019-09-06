2a71008afae85ed514db8767148b93605362e78d
Tiruhi <tir357@yandex.ru>
2019-09-06 09:17:08 +0400

    Structuring files by folders 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import MainContent from './components/MainContent/MainContent'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import MainContent from '../MainContent/MainContent'
 import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
